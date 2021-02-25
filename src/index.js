6782a033f4a5e3f5ed970fd7491575b523bbd7a4
pepe <josemperedo@gmail.com>
2021-02-24 18:32:16 -0600

    agregando bootstrap y navbar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/index.css'
+import App from './components/App'
+import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
