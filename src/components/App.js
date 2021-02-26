8d58510861e89c0640a14a7f695717bfe00ab4c3
pepe <josemperedo@gmail.com>
2021-02-25 19:17:27 -0600

    editando seccion 1 y 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import './assets/App.css'
 import Navegacion from './components/Navegacion/Navegacion'
 import Seccion1 from './components/Seccion1/Seccion1'
+
 export default function App() {
   return (
     <div className="app">
       <Navegacion/>
-      <Seccion1/>
+      <Seccion1 titulo='Aplicaciones'   p='Ipsum commodo reprehenderit minim cupidatat. tetur aute exercitation do. '/>
     </div>
   )
 }
